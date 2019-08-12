482491376cc0edd12f260ae9a73911ae30633a93
harshit-agarwal26 <54023996+harshit-agarwal26@users.noreply.github.com>
2019-08-12 16:16:49 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs= require('fs');
+
+
+ var app=express();
+ hbs.registerPartials(__dirname +'/views/partials')
+
+ app.set('view engine', 'hbs');
+
+
+ app.use((req, res, next) => {
+ 	var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+
+ 	console.log(log);
+ 	fs.appendFile('server.log',log + '\n',(err) => {
+ 		if (err){
+ 			console.log('unable to harshit append to serer logg')
+
+ 		}
+ 	});
+ 	next();
+
+
+
+  });
+ //app.use((req, res, next)=> {
+ //	res.render('maintainance.hbs');
+
+ //});
+  app.use(express.static(__dirname + '/public'));
+
+
+  app.get('/',(req, res) => {
+  	res.render('home.hbs', {
+  		pageTitle: 'home page',
+  		welcomeMessage: 'welcome to my website',
+  		currentYear: new Date().getFullYear()
+  	});
+
+  	
+
+  });
+  
+
+  app.get('/harshit', (req, res) => {
+  	res.render('about.hbs',  {
+  		pageTitle: 'about page',
+  		  		currentYear: new Date().getFullYear()
+
+  	});
+  });
+
+  app.listen(5000);
